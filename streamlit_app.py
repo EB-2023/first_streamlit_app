43b232269b2234ddfc940e9ff1e25cd256e5e93e
EB-2023 <149061057+EB-2023@users.noreply.github.com>
2023-10-26 14:15:05 +0100

    Update streamlit_app.py
    getting the app to filter the table shown based on which fruits are selected by the customer

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,11 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include. Pre-populate the list to set an example for the customer as well.
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+# Let's put a pick list here so they can pick the fruit they want to include. Pre-populate the list to set an example for the customer as well. Selected fruits pulled into a new variable.
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
+# Pull the data for the fruits selected into a new variable and this is what we will display on the page
+fruits_to_show = my_fruit_list.loc(fruits_selected)
 
 # Display the table on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
